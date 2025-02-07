1f23f943ee179b4eda6d516dc23683508822a7af
perkie01 <perkie01@email.franklin.edu>
2025-02-07 08:29:22 -0500

    Update index.php
    Style location updated.

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kamala Harris for President</title>
-    <link rel="stylesheet" href="https://ericadperkins.com/css/index.css">
-    <link rel="stylesheet" href="https://ericadperkins.com/css/style.css">
-    <link rel="stylesheet" href="https://ericadperkins.com/css/bootstrap.css">
-    <link rel="stylesheet" href="https://ericadperkins.com/css/bootstrap-grid.css">
-    <link rel="stylesheet" href="https://ericadperkins.com/css/bootstrap-utilities.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" href="css/bootstrap-utilities.css">
     <script src="js/jquery.js"></script>
     <style>
         *{
