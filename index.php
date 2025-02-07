3bb1ac7ae3ebd655cf99a6d79fb23b5adac22485
perkie01 <perkie01@email.franklin.edu>
2025-02-07 08:26:16 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kamala Harris for President</title>
+    <link rel="stylesheet" href="https://ericadperkins.com/css/index.css">
+    <link rel="stylesheet" href="https://ericadperkins.com/css/style.css">
+    <link rel="stylesheet" href="https://ericadperkins.com/css/bootstrap.css">
+    <link rel="stylesheet" href="https://ericadperkins.com/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="https://ericadperkins.com/css/bootstrap-utilities.css">
+    <script src="js/jquery.js"></script>
+    <style>
+        *{
+            background-color: #ffffff;
+            margin: 0;
+            padding: 0;
+        }
+        body{
+            width: 100vw;
+            height: 100vh;
+            overflow: none;
+        }
+        .body{
+            background: url("kamala-website-lg-background-1.png");
+            background-repeat: no-repeat;
+            background-size: cover;
+            z-index: 999;
+        }
+        body::-webkit-scrollbar {
+          display: none;
+        }
+        @media only screen and (min-width: 1920px){
+            .body{
+                background: url("kamala-website-lg-background-1.png");
+                background-repeat: no-repeat;
+                background-size: cover;
+            }
+            img[src="kamala-website-xs-background-1.png"]{
+                display: none;
+            }
+        }
+        @media only screen and (max-width: 768px) {
+            .body{
+                background: url("kamala-website-xs-background-1.png");
+                background-repeat: no-repeat;
+                background-size: cover;
+            }
+            img[src="kamala-website-lg-background-1.png"]{
+                display:none;
+            }
+        }
+    </style>
+</head>
+<body class="container-fluid body">
+    <div class="body">
+        
+    </div>
+</body>
+
+
+
+
+
+
+
+
+
